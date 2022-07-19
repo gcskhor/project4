057951c8e461b2b24e7a6cfc16f403e1fb85d64c
gcskhor <gcskhor@yahoo.com.sg>
2022-07-19 21:21:21 +0800

    added dotenv, removed comments

--- telebot.mjs
+++ telebot.mjs
@@ -1,16 +1,14 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable camelcase */
 import axios from 'axios';
+import dotenv from 'dotenv';
 
-const WHATSUP_BOT_TOKEN = '5321368970:AAGa2m9egBOy0BR4WP8R-T2VQfA-i9IyzuI';
+dotenv.config();
+
+const { WHATSUP_BOT_TOKEN } = process.env;
 
 export const updatesUrl = `https://api.telegram.org/bot${WHATSUP_BOT_TOKEN}/getUpdates?allowed_updates=["message"]`;
 
 export default function telegramNotify(userId, message) {
   const url = `https://api.telegram.org/bot${WHATSUP_BOT_TOKEN}/sendMessage?chat_id=${userId}&text=${message}&parse_mode=html`;
-
-  console.log('notifiying user on telegram');
 
   axios.get(url)
     .then((response) => {
@@ -21,20 +19,3 @@ export default function telegramNotify(userId, message) {
       console.log(error);
     });
 }
-
-// // Example
-const gerald_id = '200224220';
-// const vernessa_id = 114679229;
-// telegramNotify(gerald_id, 'WHADDUP');
-
-// axios.get(updatesUrl)
-//   .then((response) => {
-//     console.log(response.data);
-
-//     for (const el of response.data.result) {
-//       console.log(el.message);
-//     }
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
